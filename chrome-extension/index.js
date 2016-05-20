3063b6f877607747f46e2370aa099c34438f8ac3
alanclarke <hi@alz.so>
2016-05-20 21:51:14 +0100

    if editing an experiment, extension shouldtell background script to try to connect with xp

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,3 +1,26 @@
-var script = document.createElement('script')
-script.src = 'http://localhost:1234/bundle.js'
-document.body.appendChild(script)
+/* globals chrome */
+if (isEditor()) {
+  connect()
+} else {
+  inject()
+}
+
+function connect () {
+  console.log('Attempting to sync with xp')
+  chrome.runtime.sendMessage({
+    xp: true,
+    url: window.location.href
+  }, console.log)
+}
+
+function inject () {
+  var script = document.createElement('script')
+  script.src = 'http://localhost:1234/bundle.js'
+  document.body.appendChild(script)
+}
+
+function isEditor () {
+  var host = /^(localhost:3000|staging\-dashboard\.qubitproducts\.com|app\.qubit\.com)$/
+  var path = /^\/p\/\d+\/experiences\/\d+\/editor\?vid=\d+/
+  return host.test(window.location.host) && path.test(window.location.pathname + window.location.search)
+}
