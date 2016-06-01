c66234b292a1d6d00040b3ab953d62b4b54a36df
alanclarke <hi@alz.so>
2016-06-01 17:16:13 +0100

    make activation.js update triggers instead of custom_segment

--- src/server/lib/code/fetch.js
+++ src/server/lib/code/fetch.js
@@ -4,9 +4,12 @@ var variation = require('../variation').get
 module.exports = function fetchCode (data) {
   return Promise.all([iteration(data), variation(data)])
     .then(function (data) {
+      var rules = data[0].activation_rules || []
+      var customJs = rules.find(rule => rule.key === 'custom_javascript')
+      customJs = (customJs && customJs.value) || ''
       return {
         global: data[0].global_code,
-        activation: data[0].custom_segment,
+        activation: customJs,
         execution: data[1].execution_code,
         variation: data[1].custom_styles
       }
