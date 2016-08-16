eccb36e50f9095988515fd9ce090a7f61cc759a2
alanclarke <hi@alz.so>
2016-08-16 18:24:11 +0100

    don't overwrite other domain auths

--- test/test-auth.js
+++ test/test-auth.js
@@ -2,7 +2,6 @@ const path = require('path')
 const fs = require('fs-promise')
 const rewire = require('rewire')
 const auth = rewire('../src/lib/auth')
-const sinon = require('sinon')
 const {expect} = require('chai')
 const exists = require('./lib/exists')
 const xprcPath = path.join(__dirname, '.xprc')
@@ -11,7 +10,7 @@ describe('auth', function () {
   let restore, domain
   beforeEach(function () {
     domain = 'domain.com'
-    restore = auth.__set__('xprcPath', sinon.stub().returns(xprcPath))
+    restore = auth.__set__({ xprcPath: xprcPath })
   })
 
   afterEach(() => {
@@ -42,6 +41,17 @@ describe('auth', function () {
       return auth.get(domain).then(function (result) {
         expect(result).to.have.property(type, token)
       })
+    })
+    it('should not overwrite previous domain', function () {
+      return auth.set(domain + 1, type, token)
+        .then(() => Promise.all([
+          auth.get(domain),
+          auth.get(domain + 1)
+        ]))
+        .then(([auth1, auth2]) => {
+          expect(auth1).to.have.property(type, token)
+          expect(auth2).to.have.property(type, token)
+        })
     })
   })
 })
