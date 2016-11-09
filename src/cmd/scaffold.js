423b7a0a868bb6be496d3184b91e61b84bad6b46
alanclarke <hi@alz.so>
2016-11-09 15:28:03 +0000

    Improve templates

--- src/cmd/scaffold.js
+++ src/cmd/scaffold.js
@@ -1,6 +1,8 @@
+const _ = require('lodash')
 const path = require('path')
 const scaffold = require('../lib/scaffold')
 const readFiles = require('../lib/read-files')
+const fs = require('fs-promise')
 const exec = require('child_process').exec
 let CWD = process.cwd()
 
@@ -11,8 +13,31 @@ module.exports = function scaffoldFromTemplate (template) {
   }, (err) => {
     if (err) return console.error(`could not find ${template} installed on your system`)
     let templateDir = path.dirname(require.resolve(template))
-    return readFiles(templateDir).then((files) => {
-      return scaffold(CWD, files)
-    })
+    return fs.readFile(path.join(CWD, 'package.json'))
+      .then((str) => JSON.parse(str), () => {})
+      .then((pkg) => {
+        return readFiles(templateDir).then((files) => {
+          let fileMap = Object.assign({}, files)
+          // merge package.json instead of overwriting
+          fileMap['package.json'] = Object.assign({}, pkg, fileMap['package.json'])
+
+          let variations = _.get(pkg, 'meta.variations')
+
+          // if experience has known variants, seed them from template
+          if (variations && Object.keys(variations).length) {
+            _.each(pkg.meta.variations, function (variation, fileName) {
+              let meta = Object.assign({}, pkg.meta, variation)
+
+              // allow templates to contain dynamic content based on package metadata
+              fileMap[fileName + '.js'] = _.template(files['variation.js'])(meta)
+              fileMap[fileName + '.css'] = _.template(files['variation.css'])(meta)
+            })
+            delete fileMap['variation.js']
+            delete fileMap['variation.css']
+          }
+
+          return scaffold(CWD, fileMap)
+        })
+      })
   })
 }
