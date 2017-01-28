f42a22d05787053089ed184a018d9030969876b1
alanclarke <hi@alz.so>
2017-01-28 07:06:41 +0000

    Recursively scaffold template files

--- src/cmd/template.js
+++ src/cmd/template.js
@@ -12,10 +12,15 @@ const defaultMeta = {
   experienceId: 2,
   iterationId: 3,
   previewUrl: 'htt://example.com',
-  variationIsControl: false,
-  variationMasterId: 4,
-  variationId: 3
+  variations: {
+    variation: {
+      variationIsControl: false,
+      variationMasterId: 4,
+      variationId: 3
+    }
+  }
 }
+
 _.templateSettings.interpolate = /<%=([\s\S]+?)%>/g
 
 module.exports = async function fromTemplate (name) {
@@ -23,38 +28,44 @@ module.exports = async function fromTemplate (name) {
   return exec(`npm link ${name}`, {
     cwd: path.resolve(__dirname, '../../')
   }, async (err) => {
-    let template
+    let output
     try {
       if (err) return log.error(new Error(`could not find ${name} installed on your system`))
-      template = await getTemplateFiles(name)
       const pkg = await getPkg().catch(() => ({}))
-      const output = {}
 
-      if (templateHas('package.json')) output['package.json'] = mergePkg(pkg, template['package.json'])
-      if (templateHas('global.js')) output['global.js'] = _.template(template['global.js'])(pkg.meta || defaultMeta)
-      if (templateHas('triggers.js')) output['triggers.js'] = _.template(template['triggers.js'])(pkg.meta || defaultMeta)
+      output = await getTemplateFiles(name)
 
       if (hasVariations(pkg)) {
         _.each(pkg.meta.variations, (variation, filename) => {
-          if (variation.variationIsControl) return
-          const meta = pkg.meta ? Object.assign({}, pkg.meta, variation) : defaultMeta
-          if (templateHas('variation.js')) output[`${filename}.js`] = _.template(template['variation.js'])(meta)
-          if (templateHas('variation.css')) output[`${filename}.css`] = _.template(template['variation.css'])(meta)
+          if (!variation.variationIsControl) {
+            output[`${filename}.js`] = output['variation.js']
+            output[`${filename}.css`] = output['variation.css']
+          }
         })
-      } else {
-        const meta = pkg.meta ? Object.assign({}, pkg.meta) : defaultMeta
-        if (templateHas('variation.js')) output['variation.js'] = _.template(template['variation.js'])(meta)
-        if (templateHas('variation.css')) output['variation.css'] = _.template(template['variation.css'])(meta)
+        delete output['variation.js']
+        delete output['variation.css']
       }
+
+      output = resolveTemplateVariables(output, pkg.meta || defaultMeta)
+
+      if (output['package.json']) output['package.json'] = mergePkg(pkg, output['package.json'])
+
       return await scaffold(CWD, output, false)
     } catch (err) {
       log.error(err.stack)
     }
-
-    function templateHas (name) {
-      return template[name]
-    }
   })
+}
+
+function resolveTemplateVariables (files, meta) {
+  return Object.keys(files).reduce((memo, filename) => {
+    let localMeta = Object.assign({}, meta, _.get(meta, `variations.${filename.replace(/\.\w+$/, '')}`))
+    let value = typeof files[filename] === 'string'
+      ? _.template(files[filename])(localMeta)
+      : resolveTemplateVariables(files[filename], localMeta)
+    memo[filename] = value
+    return memo
+  }, {})
 }
 
 function hasVariations (pkg) {
