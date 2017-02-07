be9dd4b0155bfd71717f8e017a632d92b53879ec
alanclarke <hi@alz.so>
2017-02-07 19:51:27 +0000

    Improve logging

--- src/cmd/open.js
+++ src/cmd/open.js
@@ -1,9 +1,15 @@
-let getPkg = require('../lib/get-pkg')
-let opn = require('opn')
+const log = require('../lib/log')
+const getPkg = require('../lib/get-pkg')
+const opn = require('opn')
 
 module.exports = async function open () {
-  const pkg = await getPkg()
+  const pkg = (await getPkg().catch()) || {}
+  pkg.meta = pkg.meta || {}
   const { propertyId, experienceId } = pkg.meta
+  if (!propertyId || !experienceId) {
+    log(`sorry! this feature assumes you have already setup an experience locally`)
+    return log(`it uses the package.json metadata to open the overview page for an existing experience`)
+  }
   const dashboardURL = `https://app.qubit.com/p/${propertyId}/experiences/${experienceId}`
   console.log(`Opening experience overview page: ${dashboardURL}`)
   opn(dashboardURL, { wait: false })
