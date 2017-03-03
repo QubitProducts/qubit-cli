925c28e2e6f71c9dadb5a7e4e23bafd852c81e9a
Junyuan Xue <junyuanxue.is@gmail.com>
2017-03-03 16:55:07 +0000

    Added tests for isUrl

--- test/lib/test-is-type.js
+++ test/lib/test-is-type.js
@@ -1,12 +1,24 @@
 const { expect } = require('chai')
 const {isName, isUrl, isId, isPath, isSubmodule} = require('../../src/lib/is-type')
 
-describe('isName', function () {
-  it('should return true if string is a name', function () {
-    expect(isName('blue-template')).to.eql(true)
+describe('isType', function () {
+  describe('isName', function () {
+    it('should return true if string is a name', function () {
+      expect(isName('blue-template')).to.eql(true)
+    })
+
+    it('should return false if string is empty', function () {
+      expect(isName(' ')).to.eql(false)
+    })
   })
 
-  it('should return false if string is empty', function () {
-    expect(isName(' ')).to.eql(false)
+  describe('isUrl', function () {
+    it('should return true if string starts with "http"', function () {
+      expect(isUrl('http://isurl.com')).to.eql(true)
+    })
+
+    it('should return false if string does not start with "http"', function () {
+      expect(isUrl('//isnoturl.com')).to.eql(false)
+    })
   })
 })
