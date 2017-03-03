1941c3bb8c682eaf649bab5ee581d8f6ca981fb1
Junyuan Xue <junyuanxue.is@gmail.com>
2017-03-03 13:41:50 +0000

    Added a test for experience-filename. Green

---
+++ test/lib/test-experience-filename.js
@@ -0,0 +1,10 @@
+const { expect } = require('chai')
+const rewire = require('rewire')
+const experienceFilename = require('../../src/lib/experience-filename')
+const experienceFixture = rewire('../fixtures//experience.json')
+
+describe('experienceFilename', function () {
+  it('should return an experience filename', function () {
+    expect(experienceFilename(experienceFixture)).to.eql('456-new-styles')
+  })
+})
