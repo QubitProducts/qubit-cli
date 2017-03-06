dc9b57d6ba03037233392dda11cb892df4380393
Tobias <thale.android@gmail.com>
2017-03-06 07:40:12 +0000

    add duplicate variation command. WIP

--- src/cmd/duplicate.js
+++ src/cmd/duplicate.js
@@ -1,17 +1,12 @@
-const _ = require('lodash')
 const log = require('../lib/log')
 const getPkg = require('../lib/get-pkg')
-const mergePkg = require('../lib/merge-pkg')
 const variationService = require('../services/variation')
-const codeService = require('../services/code')
 const readFiles = require('../lib/read-files')
-const fs = require('fs-promise')
-const path = require('path')
 let CWD = process.cwd()
 
 module.exports = async function duplicate (variation) {
   try {
-    const { EXECUTION, CSS } = variationService.DEFAULTS
+  	const { EXECUTION, CSS } = variationService.DEFAULTS
     const pkg = await getPkg()
     const {propertyId, experienceId} = pkg.meta
     const variationMatch = variation.match(/\d+/g)
@@ -21,8 +16,8 @@ module.exports = async function duplicate (variation) {
     const executionCode = localFiles[`variation-${variationId}.js`]
     const cssCode = localFiles[`variation-${variationId}.css`]
 
-    const variationData = {
-      advanced_mode: true,
+    const data = {
+    	advanced_mode: true,
       custom_styles: cssCode || CSS,
       execution_code: executionCode || EXECUTION,
       experimentId: Number(experienceId),
@@ -30,39 +25,11 @@ module.exports = async function duplicate (variation) {
       name: `Variation ${variationId} copy`,
       options: null
     }
+    console.log(data)
+    const variationResponse = await variationService.duplicate(propertyId, experienceId, data)
+    console.log(variationResponse)
 
-    const variationResponse = await variationService.duplicate(propertyId, experienceId, variationData)
-    if (variationResponse) writeVariation(variationResponse)
-
-    function writeVariation (variation) {
-      const variationJS = `variation-${variation.id}.js`
-      const variationCSS = `variation-${variation.id}.css`
-      if (variation.execution_code && variation.custom_styles) {
-        fs.outputFile(path.join(CWD, variationJS), variation.execution_code)
-        fs.outputFile(path.join(CWD, variationCSS), variation.custom_styles)
-        mergePackages(`variation-${variation.id}`)
-      }
-    }
-
-    async function mergePackages (newVariation) {
-	    const remoteFiles = await codeService.get(propertyId, experienceId)
-	    const remotePkg = remoteFiles['package.json']
-	    const mergedPkg = mergePkg(pkg, remotePkg)
-	    const pkgVariations = _.get(mergedPkg, 'meta.variations')
-
-	    // Merge the new remote package.json witht he local version and clean up the local version.
-	    for (let variation in pkgVariations) {
-	    	const control = pkgVariations[variation]['variationIsControl']
-
-	    	if (!localFiles[`${variation}.js`] && !control && variation !== newVariation) {
-	    		delete pkgVariations[variation]
-	    	}
-	    }
-
-	    const cleanedPkg = JSON.stringify(mergedPkg, null, 2)
-	    fs.outputFile(path.join(CWD, 'package.json'), cleanedPkg)
-	    log(`Duplicated variation - ${variationId}`)
-    }
+    // if (_.get(pkg, 'meta')) files['package.json'] = JSON.stringify(mergePkg(pkg, files['package.json']), null, 2)
   } catch (err) {
     log.error(err)
   }
