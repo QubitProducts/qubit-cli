e8f9111d6ec0e6900e2f80dab4e7c48d172a2825
alanclarke <hi@alz.so>
2017-03-08 10:44:07 +0000

    Make pick variation pure

--- src/lib/pick-variation.js
+++ src/lib/pick-variation.js
@@ -1,7 +1,4 @@
-const {readdir} = require('fs-promise')
-
-module.exports = async function getVariationFileName (cwd) {
-  let files = await readdir(cwd)
-  files = files.filter(f => /\.js$/.test(f) && !/(triggers|global)/.test(f)).sort().reverse()
-  return files[files.length - 1]
+module.exports = async function pickVariation (names) {
+  names = names.filter(f => /\.js$/.test(f) && !/(triggers|global)/.test(f)).sort().reverse()
+  return names[names.length - 1]
 }
