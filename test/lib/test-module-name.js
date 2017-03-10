1934435d204b99e64a4c585b52d55f5634d04a60
Junyuan Xue <junyuanxue.is@gmail.com>
2017-03-10 16:49:13 +0000

    added a test for module name

--- test/lib/test-module-name.js
+++ test/lib/test-module-name.js
@@ -5,4 +5,8 @@ describe('moduleName', function () {
   it('should return the string itself if it is a path', function () {
     expect(moduleName('/path')).to.eql('/path')
   })
+
+  it('should return module name if the string is not a path', function () {
+    expect(moduleName('module-name')).to.eql('module-name')
+  })
 })
