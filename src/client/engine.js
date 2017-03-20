d2af8c211fb1521f2f50cca46f4e6cd2e5b7751e
alanclarke <hi@alz.so>
2017-03-20 22:51:32 +0000

    Ensure __qubit.amd.define = window.__qubit.xp.amd.define

--- src/client/engine.js
+++ src/client/engine.js
@@ -2,6 +2,7 @@ const _ = require('lodash')
 const log = require('./log')
 
 module.exports = function experienceEngine (options, globalFn, triggerFn, variationFn) {
+  _.set(window, '__qubit.amd.define', window.__qubit.xp.amd.define)
   log.info('running global')
   globalFn()
   log.info('running activation')
