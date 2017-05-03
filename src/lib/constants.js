a2605a7037e980f1da985643313b25e92e5a786c
alanclarke <alan@qubit.com>
2017-05-03 12:56:43 +0100

    Fixes issue where chrome rejects self signed certificate

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -3,6 +3,12 @@ const HOME = process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'
 const CERT_DIR = path.join(HOME, '.qubitcert')
 const CERT_PATH = path.join(CERT_DIR, 'qubit_serve.crt')
 const KEY_PATH = path.join(CERT_DIR, 'qubit_serve.key')
-const KEY_OPTIONS = { days: 365, selfSigned: true, organization: 'Qubit' }
+const KEY_OPTIONS = {
+  days: 365,
+  selfSigned: true,
+  organization: 'Qubit',
+  commonName: 'localhost',
+  altNames: ['localhost']
+}
 
 module.exports = { HOME, CERT_DIR, CERT_PATH, KEY_PATH, KEY_OPTIONS }
