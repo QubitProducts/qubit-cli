a40232b5a6fa4aabbab873d89e23a6956721bc66
Richard Lake <ralake1985@gmail.com>
2017-06-16 09:58:51 +0100

    Refactor to simplify user choices

--- src/cmd/duplicate.js
+++ src/cmd/duplicate.js
@@ -2,7 +2,6 @@ const _ = require('lodash')
 const chalk = require('chalk')
 const input = require('input')
 const log = require('../lib/log')
-const fs = require('fs-promise')
 const suggest = require('../lib/suggest')
 const getPkg = require('../lib/get-pkg')
 const scaffold = require('../lib/scaffold')
@@ -11,52 +10,47 @@ const cloneExperience = require('../lib/clone-experience')
 const variationService = require('../services/variation')
 const codeService = require('../services/code')
 const experienceService = require('../services/experience')
-let CWD = process.cwd()
+const cwd = process.cwd()
 
 module.exports = async function duplicate () {
   try {
     const pkg = await getPkg()
+    const propertyId = _.get(pkg, 'meta.propertyId') || await suggest.property('Select a property to duplicate from')
+    const experienceId = _.get(pkg, 'meta.experienceId') || await suggest.experience(propertyId)
+    const variations = await variationService.getAll(propertyId, experienceId)
+    const nextVariationNumber = Object.keys(variations).length
+    const variationChoices = getVariationChoices(variations)
+    const experienceChoice = { name: 'Entire experience', value: experienceId }
+    const choices = [experienceChoice]
 
-    if (!pkg.meta) {
-      await duplicateExperience()
-    } else {
-      const selectOptions = [
-        { name: 'An experience', value: 'experience' },
-        { name: 'A variation', value: 'variation' }
-      ]
-      const experienceOrVariation = await input.select('What would you like to duplicate?', selectOptions)
+    if (pkg.meta) {
+      choices.push(...variationChoices)
+      const chosenId = await input.select('What would you like to duplicate?', choices)
 
-      if (experienceOrVariation === 'experience') {
-        await duplicateExperience()
+      if (chosenId === experienceId) {
+        await duplicateExperience(propertyId, experienceId)
       } else {
-        await duplicateVariation(pkg)
+        await duplicateVariation(propertyId, experienceId, chosenId, nextVariationNumber, pkg)
       }
+    } else {
+      await duplicateExperience(propertyId, experienceId)
     }
   } catch (err) {
     log.error(err)
   }
 }
 
-async function duplicateVariation (pkg) {
-  const { propertyId, experienceId } = pkg.meta
-  const variations = pkg.meta.variations
-  const variationChoices = getVariationChoices(variations)
-  let chosenVariationId
-
-  if (variationChoices.length > 1) {
-    chosenVariationId = await input.select('Which variation would you like to duplicate?', variationChoices)
-  } else {
-    chosenVariationId = variationChoices[0].value
-  }
-
-  const variation = await variationService.get(propertyId, experienceId, chosenVariationId)
+async function duplicateVariation (propertyId, experienceId, variationId, nextVariationNumber, pkg) {
+  const variation = await variationService.get(propertyId, experienceId, variationId)
+  const defaultName = `Variation ${nextVariationNumber}`
+  const code = await variationService.getCode(variation)
 
   delete variation.id
   delete variation.master_id
 
-  variation.name = `Variation ${Object.keys(variations).length}`
-  variation.execution_code = String(await fs.readFile(`${CWD}/variation-${chosenVariationId}.js`))
-  variation.custom_styles = String(await fs.readFile(`${CWD}/variation-${chosenVariationId}.css`))
+  variation.name = await input.text('What do you want to call this variation?', { default: defaultName })
+  variation.execution_code = code[`variation-${variationId}.js`]
+  variation.custom_styles = code[`variation-${variationId}.css`]
 
   const newVariation = await variationService.create(propertyId, experienceId, variation)
   const fileName = variationService.getFilename(newVariation)
@@ -65,37 +59,35 @@ async function duplicateVariation (pkg) {
   delete pkg.meta.variations[fileName]
 
   if (_.get(pkg, 'meta')) files['package.json'] = JSON.stringify(mergePkg(pkg, files['package.json']), null, 2)
-  await scaffold(CWD, files, false, false)
+  await scaffold(cwd, files, false, false)
 }
 
-async function duplicateExperience () {
-  const propertyId = await suggest.property('Select a property')
-  const experienceId = await suggest.experience(propertyId)
+async function duplicateExperience (propertyId, experienceId) {
   const experience = await experienceService.get(propertyId, experienceId)
-  const targetPropertyId = await suggest.property('Select a property to clone the experience to')
-  const name = `${experience.name} (copy)`
+  const targetPropertyId = await suggest.property('Select a property to duplciate the experience to')
+  const name = await input.text('What do you want to call this experience?', { default: `${experience.name} (copy)` })
   const previewUrl = _.get(experience, 'recent_iterations.draft.url')
   const duplicateOptions = { id: experienceId, name, previewUrl, targetPropertyId }
   const duplicatedExperience = await experienceService.duplicate(propertyId, duplicateOptions)
 
   if (duplicatedExperience) {
     log(chalk.green('Experience successfully duplicated'))
-    const shouldClone = await input.confirm('Do you want to clone the experience into the current directory?')
+    const shouldClone = await input.confirm('Do you want to clone the duplicated experience into the current directory?')
 
-    if (shouldClone) await cloneExperience(CWD, targetPropertyId, duplicatedExperience.id)
+    if (shouldClone) await cloneExperience(cwd, targetPropertyId, duplicatedExperience.id)
   } else {
     log(chalk.red('Experience could not be duplicated'))
   }
 }
 
 function getVariationChoices (variations) {
-  const allVariations = Object.keys(variations).map((variation, index) => {
+  const controlKey = Object.keys(variations)[0]
+  const allVariations = _.omit(variations, controlKey)
+
+  return _.map(allVariations, variation => {
     return {
-      name: `Variation ${index}`,
-      value: Number(variation.match(/variation-(\d+)/i)[1]) // variationMasterId
+      name: variation.name,
+      value: variation.master_id
     }
   })
-
-  // return all variations apart from control
-  return _.drop(allVariations, 1)
 }
