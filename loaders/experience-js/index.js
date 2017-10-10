f0e251535ed0c14a279e8e35ce4a900105084032
alanclarke <alan@qubit.com>
2017-10-10 14:35:42 +0100

    Parity with how platform handles dependencies at runtime

--- loaders/experience-js/index.js
+++ loaders/experience-js/index.js
@@ -1,52 +1,11 @@
 const path = require('path')
-const hasNoCode = require('../../src/lib/hasNoCode')
-const { TRIGGERS, EXECUTION } = require('../../src/lib/constants')
+const rewrite = require('./lib/rewrite')
 
 module.exports = function loader (content, { file }) {
-  let filename = path.basename(file)
-
-  let deps = getDeps()
-
-  // Assume a blank file means default empty function
-  // This is a better experience than showing errors
-  // and when pushing we revert to default function in that case anyway
-  let code = content
-
-  if (hasNoCode(code)) {
-    if (filename.includes('triggers')) code = TRIGGERS
-    if (filename.includes('variation')) code = EXECUTION
-  }
-
-  code = addModuleExports(code)
-
-  code = addAMD(code)
-
-  for (let dep of deps) code = allowRealRequires(code, dep)
-
-  return code
-}
-
-function addModuleExports (code) {
-  return code.includes('module.exports')
-    ? code
-    : 'module.exports = ' + code
-}
-
-function addAMD (code) {
-  return code
-    .replace(/(^|\s+)require\s*\(/g, '$1window.__qubit.xp.amd.require(')
-    .replace(/=\s*require\s*(\n|,|;|$)/g, '= window.__qubit.xp.amd.require$1')
-}
-
-function allowRealRequires (code, dep) {
-  return code.replace(new RegExp(`window\\.__qubit\\.xp\\.amd\\.require\\((['"])${dep}`, 'gi'), `require($1${dep}`)
-}
-
-function getDeps () {
-  let deps = {}
+  let hasDeps = false
   try {
     let pkg = require(path.join(process.cwd(), 'package.json'))
-    deps = pkg.dependencies || {}
+    hasDeps = pkg.dependencies
   } catch (e) {}
-  return Object.keys(deps)
+  return rewrite(content, file, hasDeps)
 }
