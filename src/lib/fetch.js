ed54d8398688980756d0d9ebb745a4dd07797451
Alexander Esp <alexjesp@gmail.com>
2017-10-12 14:39:12 +0100

    Improve lib/fetch error logging

--- src/lib/fetch.js
+++ src/lib/fetch.js
@@ -5,8 +5,6 @@ const log = require('./log')
 const ensureToken = require('./ensure-token')
 const login = require('../server/lib/login')
 const { ID_TOKEN } = require('./constants')
-const NOT_FOUND = new Error('NOT_FOUND, the experience you requested does not exist')
-const OUT_OF_DATE = new Error('OUT_OF_DATE, your local copy of the experience is out of date')
 
 module.exports = {
   get: fetchWithAuth('GET'),
@@ -18,6 +16,7 @@ function fetchWithAuth (method) {
   return async function fetch (path, data, isRetry) {
     let headers, token
     const idToken = await qubitrc.get(ID_TOKEN)
+    const url = config.services.app + path
 
     if (idToken) {
       try {
@@ -30,11 +29,17 @@ function fetchWithAuth (method) {
 
     if (!headers) return login().then(() => fetch(path, data))
 
-    return axios(config.services.app + path, { method, data, headers }).then(handler, ({response}) => handler(response))
+    return axios(url, { method, data, headers }).then(handler, ({response}) => handler(response))
 
     function handler (resp) {
-      if (resp.status === 422) throw OUT_OF_DATE
-      if (resp.status === 404) throw NOT_FOUND
+      if (resp.status === 422) {
+        log.error(`Unprocessable entity at url ${url}`)
+        return process.exit(1)
+      }
+      if (resp.status === 404) {
+        log.error(`Nothing found at url ${url}`)
+        return process.exit(1)
+      }
       if (resp.data === 'Unauthorized' || (typeof resp.data === 'string' && resp.data.includes('login.css'))) {
         if (isRetry) {
           log.error('Credentials rejected after several attempts')
