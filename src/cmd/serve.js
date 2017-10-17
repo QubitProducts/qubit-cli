2cd3a054fd2734002513036da6e2bd97e6d9a4e8
alanclarke <alan@qubit.com>
2017-10-17 18:04:51 +0100

    Fix serve

--- src/cmd/serve.js
+++ src/cmd/serve.js
@@ -1,3 +1,3 @@
 const server = require('../server/lib/serve')
 
-module.exports = (variation, opts) => server(Object.assign(opts, { variation }))
+module.exports = (variationFileName, opts) => server(Object.assign(opts, { variationFileName }))
