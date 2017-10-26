e4f01e7b111ef32fd0fd134fa29f299067de9b87
alanclarke <alan@qubit.com>
2017-10-26 10:02:26 +0100

    Adding try catch around a few commands that were missing it

--- src/cmd/push.js
+++ src/cmd/push.js
@@ -9,33 +9,37 @@ const diff = require('./diff')
 let CWD = process.cwd()
 
 module.exports = async function push (options) {
-  const pkg = await getPkg()
-  const {propertyId, experienceId} = (pkg.meta || {})
-  if (!propertyId || !experienceId) return log.info('Nothing to push')
+  try {
+    const pkg = await getPkg()
+    const {propertyId, experienceId} = (pkg.meta || {})
+    if (!propertyId || !experienceId) return log.info('Nothing to push')
 
-  if (!options.force) {
-    let { files, experience } = await down(experienceId)
-    if (experience.solution_id === 7) {
-      throw new Error('qubit-cli does not support simple message experiences')
+    if (!options.force) {
+      let { files, experience } = await down(experienceId)
+      if (experience.solution_id === 7) {
+        throw new Error('qubit-cli does not support simple message experiences')
+      }
+
+      let remotePkg = JSON.parse(files['package.json'])
+      let remoteExperienceUpdatedAt = remotePkg.meta.remoteUpdatedAt
+      let remoteVariantsUpdatedAt = _.map(remotePkg.meta.variations, v => v.remoteUpdatedAt)
+      let remoteUpdatedAts = [remoteExperienceUpdatedAt].concat(remoteVariantsUpdatedAt)
+
+      let localExperienceUpdatedAt = pkg.meta.remoteUpdatedAt
+      let localVariantsUpdatedAt = _.map(pkg.meta.variations, v => v.remoteUpdatedAt)
+      let localUpdatedAts = [localExperienceUpdatedAt].concat(localVariantsUpdatedAt)
+
+      if (remoteUpdatedAts.join('|') !== localUpdatedAts.join('|')) {
+        log.info('Remote has changed since the last interaction!')
+        return diff()
+      }
     }
 
-    let remotePkg = JSON.parse(files['package.json'])
-    let remoteExperienceUpdatedAt = remotePkg.meta.remoteUpdatedAt
-    let remoteVariantsUpdatedAt = _.map(remotePkg.meta.variations, v => v.remoteUpdatedAt)
-    let remoteUpdatedAts = [remoteExperienceUpdatedAt].concat(remoteVariantsUpdatedAt)
-
-    let localExperienceUpdatedAt = pkg.meta.remoteUpdatedAt
-    let localVariantsUpdatedAt = _.map(pkg.meta.variations, v => v.remoteUpdatedAt)
-    let localUpdatedAts = [localExperienceUpdatedAt].concat(localVariantsUpdatedAt)
-
-    if (remoteUpdatedAts.join('|') !== localUpdatedAts.join('|')) {
-      log.info('Remote has changed since the last interaction!')
-      return diff()
-    }
+    log.info('Pushing...')
+    await codeService.set(propertyId, experienceId, await readFiles(CWD))
+    await updatePkg(experienceId)
+    log.info('Pushed!')
+  } catch (err) {
+    log.error(err)
   }
-
-  log.info('Pushing...')
-  await codeService.set(propertyId, experienceId, await readFiles(CWD))
-  await updatePkg(experienceId)
-  log.info('Pushed!')
 }
