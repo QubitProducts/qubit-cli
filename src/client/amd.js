926d3306a4959ecb2ae502518c3f5df703226b5b
alanclarke <alan@qubit.com>
2017-10-26 17:21:57 +0100

    Add more dependencies

--- src/client/amd.js
+++ src/client/amd.js
@@ -1,13 +1,17 @@
 const _ = require('lodash')
 const microAmd = require('micro-amd')
 const map = {
-  '@qubit/http-api-tally': require('@qubit/http-api-tally'),
+  '@qubit/abandonment-recovery': require('@qubit/abandonment-recovery'),
+  '@qubit/datasets': require('@qubit/datasets'),
   '@qubit/advanced-modal': require('@qubit/advanced-modal'),
   '@qubit/email-scheduler': require('@qubit/email-scheduler'),
   '@qubit/exit-checker': require('@qubit/exit-checker'),
   '@qubit/floating-panel': require('@qubit/floating-panel'),
   '@qubit/growl': require('@qubit/growl'),
+  '@qubit/http-api-tally': require('@qubit/http-api-tally'),
+  '@qubit/http-api-stash': require('@qubit/http-api-stash'),
   '@qubit/image-carousel': require('@qubit/image-carousel'),
+  '@qubit/message-scheduler': require('@qubit/message-scheduler'),
   '@qubit/jquery': require('@qubit/jquery'),
   '@qubit/mvt': require('@qubit/mvt'),
   '@qubit/poller': require('@qubit/poller'),
@@ -19,10 +23,11 @@ const map = {
   '@qubit/uv-api': require('@qubit/uv-api'),
   '@qubit/visitor-pulse': require('@qubit/visitor-pulse'),
   'cookieman': require('cookieman'),
-  'preact': require('preact'),
   'jquery': require('@qubit/jquery'),
+  'preact': require('preact'),
+  'slapdash': require('slapdash'),
   'sync-p': require('sync-p/extra'),
-  'slapdash': require('slapdash')
+  'qubit-react/experience': require('qubit-react/experience')
 }
 
 module.exports = function createAMD () {
