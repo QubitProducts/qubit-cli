571e60aea6a80f0744e9e7849f79733fa3bf344e
alanclarke <alan@qubit.com>
2018-01-16 11:23:39 +0000

    Reuse existing user/property endpoint + memoize it

--- src/services/property.js
+++ src/services/property.js
@@ -1,11 +1,12 @@
 const fetch = require('../lib/fetch')
+const memoize = require('p-memoize')
 
-function get (propertyId) {
-  return fetch.get(`/api/properties/${propertyId}`)
-}
-
-function getAll () {
+const getAll = memoize(async function getAll () {
   return fetch.get('/api/user/properties')
+})
+
+async function get (propertyId) {
+  return getAll().then(all => all.find(p => p.id === propertyId))
 }
 
 module.exports = { get, getAll }
