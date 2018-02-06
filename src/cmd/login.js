6f8b83ec4e3583d06ed71f07123f799950dd56a1
alanclarke <alan@qubit.com>
2018-02-06 15:31:12 +0000

    Better login

--- src/cmd/login.js
+++ src/cmd/login.js
@@ -1,8 +1,10 @@
 const login = require('../server/lib/login')
+const qubitrc = require('../lib/qubitrc')
 const log = require('../lib/log')
 
 module.exports = async function loginCmd (id) {
   try {
+    await qubitrc.logout()
     await login()
     log.info('Login successful!')
   } catch (err) {
