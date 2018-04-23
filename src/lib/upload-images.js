1860d19ff1a2df4d436f256576e235d4718716eb
Sam Thomas <samuelfullerthomas@gmail.com>
2018-04-23 15:31:15 +0100

    prompt for property related to the image you're uploading

--- src/lib/upload-images.js
+++ src/lib/upload-images.js
@@ -1,13 +1,15 @@
 const fetch = require('./fetch')
+const suggest = require('./suggest')
 const FormData = require('form-data')
 var fs = require('fs')
 
 module.exports = async function uploadImages (files) {
-  if (!Array.isArray(files)) throw new Error(`uploadImages expects an array`)
+  if (!Array.isArray(files)) throw new Error(`uploadImages expects at least one image to be passed`)
+  const propertyId = await suggest.property()
   const fileUrls = files.map(file => {
     const data = new FormData()
     data.append(`image[file]`, fs.createReadStream(file))
-    return fetch.post(`/p/2500/assets/images`, data, {
+    return fetch.post(`/p/${propertyId}/assets/images`, data, {
       headers: data.getHeaders()
     })
   })
