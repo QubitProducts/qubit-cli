7d644c4927c22edc5d2f11c7a727e643f1238166
Sam Thomas <samuelfullerthomas@gmail.com>
2018-04-23 15:51:25 +0100

    Check for propertyId from the package.json first

--- src/lib/upload-images.js
+++ src/lib/upload-images.js
@@ -1,11 +1,15 @@
+const _ = require('slapdash')
 const fetch = require('./fetch')
 const suggest = require('./suggest')
+const getPkg = require('./get-pkg')
 const FormData = require('form-data')
 var fs = require('fs')
 
 module.exports = async function uploadImages (files) {
   if (!Array.isArray(files)) throw new Error(`uploadImages expects at least one image to be passed`)
-  const propertyId = await suggest.property()
+  const pkg = await getPkg()
+  let propertyId = _.get(pkg, 'meta.propertyId')
+  if (!propertyId) propertyId = await suggest.property()
   const fileUrls = files.map(file => {
     const data = new FormData()
     data.append(`image[file]`, fs.createReadStream(file))
