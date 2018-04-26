5eb644a7385a35a462b873211e00082e0c5b25ca
alanclarke <alan@qubit.com>
2018-04-26 14:32:13 +0100

    Don't let templates override base attributes like name and description & enforce connonical key order

--- src/lib/merge-pkg.js
+++ src/lib/merge-pkg.js
@@ -9,8 +9,73 @@ module.exports = function mergePkg (localPkg, remotePkg) {
   const pkg = { ...localPkg, ...remotePkg }
   _.set(pkg, 'meta', _.merge({}, localPkg.meta, remotePkg.meta) || {})
   _.set(pkg, 'meta.templates', _.uniq(getTemplates(localPkg).concat(getTemplates(remotePkg))))
+  ;[
+    'dependencies',
+    'devDependencies',
+    'peerDependencies',
+    'bundledDependencies',
+    'bundleDependencies',
+    'optionalDependencies'
+  ].forEach(key => pkg[key] && remotePkg[key] && _.set(pkg, key, { ...localPkg[key], ...remotePkg[key] }))
+
   if (_.get(remotePkg, 'meta.variations')) pkg.meta.variations = remotePkg.meta.variations
-  return pkg
+  const {
+    name,
+    version,
+    description,
+    keywords,
+    homepage,
+    bugs,
+    license,
+    author,
+    contributors,
+    files,
+    main,
+    bin,
+    man,
+    directories,
+    repository,
+    scripts,
+    browser,
+    dependencies,
+    devDependencies,
+    peerDependencies,
+    bundledDependencies,
+    bundleDependencies,
+    optionalDependencies,
+    engines,
+    engineStrict,
+    meta
+  } = pkg
+  return _.omitBy({
+    name,
+    version,
+    description,
+    keywords,
+    homepage,
+    bugs,
+    license,
+    author,
+    contributors,
+    files,
+    main,
+    bin,
+    man,
+    directories,
+    repository,
+    scripts,
+    browser,
+    dependencies,
+    devDependencies,
+    peerDependencies,
+    bundledDependencies,
+    bundleDependencies,
+    optionalDependencies,
+    engines,
+    engineStrict,
+    ...pkg,
+    meta
+  }, _.isUndefined)
 }
 
 function getTemplates (pkg) {
