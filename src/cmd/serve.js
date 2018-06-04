51e9815be72facda5969f2d7ed6f2d8498be786f
alanclarke <alan@qubit.com>
2018-06-04 10:30:28 +0100

    Fix targeting different variation

--- src/cmd/serve.js
+++ src/cmd/serve.js
@@ -1,3 +1,3 @@
 const server = require('../server/lib/serve')
 
-module.exports = (variationFileName, opts) => server(Object.assign(opts, { variationFileName }))
+module.exports = (variationFileName, opts) => server({ ...opts, variationFileName })
