9c3dda94bce118b28e62c6a7413c2fad57108748
Sam Thomas <samuelfullerthomas@gmail.com>
2018-08-14 12:23:45 +0100

    resolve merge conflicts

--- src/cmd/create-template.js
+++ src/cmd/create-template.js
@@ -4,34 +4,30 @@ const log = require('../lib/log')
 const CWD = process.cwd()
 
 const { getPropertyId } = require('../lib/get-property-and-experience-ids')
-const createTemplate = require('../lib/create-template')
+const getTemplate = require('../lib/create-template')
 
-module.exports = async function create (pid) {
-  try {
-    const propertyId = await getPropertyId(pid)
-    if (!propertyId) {
-      log.info(`PropertyId not found, are you in an template folder?`)
-      return
-    }
-
-    const name = clean(await input.text(
-      formatLog('   What would you like to call your template?'), {
-        validate (name) {
-          if (name.length > 2) return true
-          return `Template names must be greater than 2 charachters long`
-        }
-      }
-    ))
-
-    const description = clean(await input.text(
-      formatLog('   Write a description for your template?'),
-      { default: 'This template was created with qubit-cli' }
-    ))
-
-    await createTemplate(CWD, propertyId, name, description)
-  } catch (err) {
-    log.error(err)
+module.exports = async function createTemplate (propertyId) {
+  propertyId = await getPropertyId(propertyId)
+  if (!propertyId) {
+    log.info(`PropertyId not found, are you in an template folder?`)
+    return
   }
+
+  const name = clean(await input.text(
+    formatLog('   What would you like to call your template?'), {
+      validate (name) {
+        if (name.length > 2) return true
+        return `Template names must be greater than 2 charachters long`
+      }
+    }
+  ))
+
+  const description = clean(await input.text(
+    formatLog('   Write a description for your template?'),
+    { default: 'This template was created with qubit-cli' }
+  ))
+
+  return getTemplate(CWD, propertyId, name, description)
 }
 
 function clean (str) {
