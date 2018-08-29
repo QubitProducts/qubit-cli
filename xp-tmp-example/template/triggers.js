b4c37f63af5c99b212736883fd85f44c3f8cf1c8
alanclarke <alan@qubit.com>
2018-08-29 17:22:41 +0100

    Remove example template

--- xp-tmp-example/template/triggers.js
+++
@@ -1,7 +0,0 @@
-function triggers (options, cb) {
-  options.log.info('Hello from triggers')
-  cb()
-  return {
-    remove: () => options.log.info('Cleaning up triggers')
-  }
-}
--- xp-tmp-example/template/variation.js
+++
@@ -1,6 +0,0 @@
-function execution (options) {
-  options.log.info('Hello from variation')
-  return {
-    remove: () => options.log.info('Cleaning up variation')
-  }
-}
