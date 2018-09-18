5ce5ec0ce5615e5c5fbd7ff2672fa55119e99a86
Alexander Esp <alexjesp@gmail.com>
2018-09-18 10:02:34 +0100

    Dont check experience status if publishing template

--- src/lib/experience-status.js
+++ src/lib/experience-status.js
@@ -2,6 +2,9 @@ const log = require('../lib/log')
 const experienceService = require('../services/experience')
 
 module.exports = async function experienceStatus (propertyId, experienceId) {
+  const experience = await experienceService.get(experienceId)
+  if (experience.is_template) return
+
   const experienceState = await experienceService.status(propertyId, experienceId)
 
   if (experienceState.processing) {
