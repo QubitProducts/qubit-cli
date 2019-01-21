89942b59566c17849c45345a64e29e743bb1f01a
alanclarke <alan@qubit.com>
2019-01-21 18:33:05 +0000

    Make pull experience accept iterationId

--- src/services/down.js
+++ src/services/down.js
@@ -5,11 +5,11 @@ const codeService = require('./code')
 const mergePkg = require('../lib/merge-pkg')
 const getPkg = require('../lib/get-pkg')
 
-module.exports = async function down (experienceId) {
+module.exports = async function down (experienceId, iterationId) {
   const pkg = await getPkg()
   const experience = await experienceService.get(experienceId)
-  const iteration = await iterationService.get(experience.last_iteration_id)
-  const variations = await variationService.getAll(experience.last_iteration_id)
+  const iteration = await iterationService.get(iterationId || experience.last_iteration_id)
+  const variations = await variationService.getAll(iterationId || experience.last_iteration_id)
   const files = codeService.getCode(experience, iteration, variations)
   files['package.json'] = JSON.stringify(mergePkg(pkg || {}, files['package.json']), null, 2)
   return { experience, iteration, variations, files }
