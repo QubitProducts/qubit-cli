3990a67ec34e6836dd0714967fd8c5e4b1c93fe1
alanclarke <alan@qubit.com>
2019-01-28 14:04:22 +0000

    Ask user if they would like to save their work before pulling down an old iteration

--- src/cmd/iterations.js
+++ src/cmd/iterations.js
@@ -2,6 +2,8 @@ const { getPropertyAndExperienceIds, getIterationId } = require('../lib/get-reso
 const { isId } = require('../lib/is-type')
 const getPkg = require('../lib/get-pkg')
 const pullExperience = require('../lib/pull-experience')
+const push = require('./push')
+const confirmer = require('confirmer')
 const CWD = process.cwd()
 
 module.exports = async function iterations (propertyId, experienceId, iterationId) {
@@ -10,6 +12,10 @@ module.exports = async function iterations (propertyId, experienceId, iterationI
 
   // Choose an iteration
   if (!isId(iterationId)) iterationId = await getIterationId(experienceId)
+
+  if (await confirmer(`This command may overwrite your current work, would you like to push first? (y/n)`)) {
+    await push()
+  }
 
   return pullExperience(CWD, propertyId, experienceId, iterationId)
 }
