3d48db874e983d920c268ed12b286cdd049461e7
alanclarke <alan@qubit.com>
2019-06-12 10:34:29 +0100

    Undo emit uv while jolt pr is being reviewed

--- src/client/amd.js
+++ src/client/amd.js
@@ -1,10 +1,9 @@
+var _ = require('slapdash')
 var microAmd = require('micro-amd')
 
 module.exports = function createAMD () {
   const amd = microAmd({ base: '//d22rutvoghj3db.cloudfront.net/' })
-  window.__qubit = window.__qubit || {}
-  window.__qubit.cli = window.__qubit.cli || {}
-  window.__qubit.cli.amd = amd
+  _.set(window, '_qubit.cli.amd', amd)
   Object.defineProperty(window.__qubit, 'amd', { get: () => amd })
   return amd
 }
