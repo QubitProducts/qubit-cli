a63d0db14c924282ca57960bc0ea406a5b8fe3b5
alanclarke <alan@qubit.com>
2019-06-12 11:03:32 +0100

    Use actual uv

--- src/client/emit-custom-goal.js
+++ src/client/emit-custom-goal.js
@@ -1,14 +1,14 @@
 module.exports = function createEmitCustomGoal (uv, meta) {
   return function emitCustomGoal (goalType, goal, goalAmount, transactionId) {
-    // uv.emit('qubit.goalAchieved', {
-    //   goalId: goal.goalId,
-    //   experienceId: meta.experimentId,
-    //   variationMasterId: meta.variationMasterId,
-    //   iterationId: meta.iterationId,
-    //   isControl: meta.variationIsControl,
-    //   goalType: goalType,
-    //   goalAmount: goalAmount,
-    //   transactionId: transactionId
-    // })
+    uv.emit('qubit.goalAchieved', {
+      goalId: goal.goalId,
+      experienceId: meta.experimentId,
+      variationMasterId: meta.variationMasterId,
+      iterationId: meta.iterationId,
+      isControl: meta.variationIsControl,
+      goalType: goalType,
+      goalAmount: goalAmount,
+      transactionId: transactionId
+    })
   }
 }
--- src/client/emit-metric.js
+++ src/client/emit-metric.js
@@ -1,15 +1,15 @@
 module.exports = function createEmitMetric (uv, meta) {
   return function createEmitMetric (type, productId, metadata) {
-    // uv.emit('qubit.metric', {
-    //   type: type,
-    //   experienceId: meta.experimentId,
-    //   iterationId: meta.iterationId,
-    //   variationId: meta.variationId,
-    //   variationMasterId: meta.variationMasterId,
-    //   productId: productId,
-    //   metadata: metadata
-    //     ? (typeof metadata === 'string' ? metadata : JSON.stringify(metadata))
-    //     : metadata
-    // })
+    uv.emit('qubit.metric', {
+      type: type,
+      experienceId: meta.experimentId,
+      iterationId: meta.iterationId,
+      variationId: meta.variationId,
+      variationMasterId: meta.variationMasterId,
+      productId: productId,
+      metadata: metadata
+        ? (typeof metadata === 'string' ? metadata : JSON.stringify(metadata))
+        : metadata
+    })
   }
 }
