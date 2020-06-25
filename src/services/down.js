0754f075b33a856e4a1d05ebd6040e16f61218c4
alanclarke <alan@qubit.com>
2020-06-25 17:19:50 +0100

    Fix

--- src/services/down.js
+++ src/services/down.js
@@ -1,3 +1,4 @@
+const propertyServicve = require('./property')
 const experienceService = require('./experience')
 const iterationService = require('./iteration')
 const variationService = require('./variation')
@@ -10,12 +11,13 @@ const getPkg = require('../lib/get-pkg')
 module.exports = async function down (experienceId, iterationId) {
   const pkg = await getPkg()
   const experience = await experienceService.get(experienceId)
+  const property = await propertyServicve.get(experience.property_id)
   iterationId = iterationId || experience.last_iteration_id
   const iteration = await iterationService.get(iterationId)
   const goals = await goalsService.get(iterationId)
   const qfns = await qfnService.get(iterationId)
   const variations = await variationService.getAll(iterationId)
-  const files = codeService.getCode(experience, iteration, goals, qfns, variations)
+  const files = codeService.getCode(property, experience, iteration, goals, qfns, variations)
   files['package.json'] = JSON.stringify(mergePkg(pkg || {}, files['package.json']), null, 2)
   return { experience, iteration, variations, files }
 }
