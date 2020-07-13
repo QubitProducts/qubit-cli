756cca8f56b98691fca2127809dba95e27770db8
Oliver Woodings <oliver.woodings@gmail.com>
2020-07-13 10:32:08 +0100

    Skip the ui login flow if there is a CI token

--- src/cmd/login.js
+++ src/cmd/login.js
@@ -6,12 +6,14 @@ const log = require('../lib/log')
 
 module.exports = async function loginCmd (id) {
   await qubitrc.logout()
-  const token = await getToken(
-    'id_token',
-    'openid profile',
-    'You are logged in and can now close this tab!'
-  )
-  await qubitrc.set(ID_TOKEN, token)
+  if (!process.env.QUBIT_TOKEN) {
+    const token = await getToken(
+      'id_token',
+      'openid profile',
+      'You are logged in and can now close this tab!'
+    )
+    await qubitrc.set(ID_TOKEN, token)
+  }
   await getRegistryToken(true)
   log.info('Login successful!')
 }
