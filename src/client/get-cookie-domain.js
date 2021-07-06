e2838a675163fb99146b97d4a8996848d889e061
alanclarke <alan@qubit.com>
2021-07-06 09:44:30 +0100

    Serve demo placements

--- src/client/get-cookie-domain.js
+++ src/client/get-cookie-domain.js
@@ -1,8 +1,9 @@
 const _ = require('slapdash')
 
-module.exports = function domain (domains, currentHost) {
-  currentHost = currentHost || window.location.hostname
-
+module.exports = function domain (
+  domains = [window.location.hostname],
+  currentHost = window.location.hostname
+) {
   const match = _.find(domains, function (domain) {
     const rDomain = regexify(domain)
     return rDomain.test(currentHost)
