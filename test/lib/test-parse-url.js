996b71fe75bac0533ce4f9b48ed014fc909d9ebd
alanclarke <alan@qubit.com>
2021-07-27 12:09:17 +0100

    Paste placement urls

--- test/lib/test-parse-url.js
+++ test/lib/test-parse-url.js
@@ -1,27 +1,35 @@
-const parseUrl = require('../../src/lib/parse-url')
+const {
+  parseExperienceUrl,
+  parsePlacementUrl
+} = require('../../src/lib/parse-url')
 const { expect } = require('chai')
 
-describe('parse url', function () {
-  it('should parse the url', function () {
-    expect(
-      parseUrl('https://app.qubit.com/p/1234/experiences/5678/editor?vid=890')
-    ).to.eql({
-      experienceId: 5678,
-      propertyId: 1234
+describe('parseExperienceUrl', function () {
+  ;[
+    'https://app.qubit.com/p/1234/experiences/5678/editor?vid=890',
+    'https://staging-dashboard.qubitproducts.com/p/1234/experiences/5678/editor?vid=890',
+    'http://localhost:3000/p/1234/experiences/5678/editor?vid=890'
+  ].forEach(url => {
+    it(`should parse ${url}`, function () {
+      expect(parseExperienceUrl(url)).to.eql({
+        experienceId: 5678,
+        propertyId: 1234
+      })
     })
-    expect(
-      parseUrl(
-        'https://staging-dashboard.qubitproducts.com/p/1234/experiences/5678/editor?vid=890'
-      )
-    ).to.eql({
-      experienceId: 5678,
-      propertyId: 1234
-    })
-    expect(
-      parseUrl('http://localhost:3000/p/1234/experiences/5678/editor?vid=890')
-    ).to.eql({
-      experienceId: 5678,
-      propertyId: 1234
+  })
+})
+
+describe('parsePlacementUrl', function () {
+  ;[
+    'https://app.qubit.com/p/1234/atom/placements/-RY8Zq-4SjOr6nauMen2Yg/create?step=editor&view=schema',
+    'https://app-staging.qubit.com/p/1234/atom/placements/-RY8Zq-4SjOr6nauMen2Yg/create?step=editor&view=schema',
+    'http://localhost:3000/p/1234/atom/placements/-RY8Zq-4SjOr6nauMen2Yg/create?step=editor&view=schema'
+  ].forEach(url => {
+    it(`should parse ${url}`, function () {
+      expect(parsePlacementUrl(url)).to.eql({
+        placementId: '-RY8Zq-4SjOr6nauMen2Yg',
+        propertyId: 1234
+      })
     })
   })
 })
