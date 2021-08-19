7d355178916421e19a2e53fd99f00c84c75d5fff
Alan Clarke <alan@qubit.com>
2021-08-19 10:19:40 +0100

    Fix clone command with no args (#440)

--- src/cmd/placement/clone.js
+++ src/cmd/placement/clone.js
@@ -18,7 +18,7 @@ module.exports = async function clone (
   isOneOf(implementationType)
   const pkg = await getPkg()
   const propertyId = await getPropertyId(propertyIdorUrl, pkg)
-  placementId = await getPlacementId(propertyIdorUrl, placementId, pkg)
+  placementId = await getPlacementId(propertyId, placementId, pkg)
   const files = await placementService.addHelpers(
     await placementService.get(propertyId, placementId)
   )
