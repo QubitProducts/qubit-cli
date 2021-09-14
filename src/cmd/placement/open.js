3f84246f8e538a9a33edf0a790956eb9a3419e02
Livia Rett <livia@qubit.com>
2021-09-14 11:41:26 +0100

    Update open command to not wait

--- src/cmd/placement/open.js
+++ src/cmd/placement/open.js
@@ -1,17 +1,12 @@
-const config = require('config')
-const throwIf = require('../../lib/throw-if')
-const getPkg = require('../../lib/get-pkg')
-const log = require('../../lib/log')
-const opn = require('opn')
-
-module.exports = async function open (page = 'editor', options) {
+module.exports = async function open (
+  page = 'editor',
+  options = { wait: false }
+) {
   await throwIf.placement('open')
   const pkg = await getPkg()
   const { propertyId, placementId } = pkg.meta
 
-  const url = `${
-    config.services.app
-  }/p/${propertyId}/atom/placements/${placementId}/create?step=editor&view=schema`
+  const url = `${config.services.app}/p/${propertyId}/atom/placements/${placementId}/create?step=${page}&view=schema`
   log.info(`opening ${url}`)
-  return opn(url)
+  return opn(url, options)
 }
