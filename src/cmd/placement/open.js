d3357c093488e8734228eab651c59fc64cdc6916
Livia Rett <livia@qubit.com>
2021-09-14 12:04:52 +0100

    Revert "Update open command to not wait"
    This reverts commit 3f84246f8e538a9a33edf0a790956eb9a3419e02.

--- src/cmd/placement/open.js
+++ src/cmd/placement/open.js
@@ -1,12 +1,17 @@
-module.exports = async function open (
-  page = 'editor',
-  options = { wait: false }
-) {
+const config = require('config')
+const throwIf = require('../../lib/throw-if')
+const getPkg = require('../../lib/get-pkg')
+const log = require('../../lib/log')
+const opn = require('opn')
+
+module.exports = async function open (page = 'editor', options) {
   await throwIf.placement('open')
   const pkg = await getPkg()
   const { propertyId, placementId } = pkg.meta
 
-  const url = `${config.services.app}/p/${propertyId}/atom/placements/${placementId}/create?step=${page}&view=schema`
+  const url = `${
+    config.services.app
+  }/p/${propertyId}/atom/placements/${placementId}/create?step=editor&view=schema`
   log.info(`opening ${url}`)
-  return opn(url, options)
+  return opn(url)
 }
